135fd7ec467061deef63ef0dd0f5dd1b126a6c2c
j4n9k4 <janukananayakkara@gmail.com>
2025-01-20 17:32:00 +0530

    Finished Task 1

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,18 @@
 fun main()
 {
+    val morningNotifications = 51
+    val eveningNotifications = 135
+    var numberOfNotifications = morningNotifications
+    for (x in 1..2)
+    {
+        printNotificationSummary(numberOfNotifications)
+        numberOfNotifications = eveningNotifications
+    }
+
+}
+
+fun printNotificationSummary(numberOfNotifications: Int)
+{
+    if(numberOfNotifications < 100 ) println("You have $numberOfNotifications notifications") else println("You have 99+ notifications")
 
 }
